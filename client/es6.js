56e6110fc5103bb695ec3c1bb809a8967eff958a
jesse <jsoldat@hotmail.com>
2017-03-10 19:08:00 +0700

    this with es6

--- client/es6.js
+++ client/es6.js
@@ -1,28 +1,52 @@
 class Counter extends React.Component {
 	constructor(props) {
 	  super(props);
-	
+	  //'this' is not allowed before super()
+		//console.log(props); //Object {text: "Hello World"}
 	  this.state = { count: 0 };
 
-	  this.click= this.click.bind(this);
+	  this.bindClickConstructor = this.bindClickConstructor.bind(this);
+	  //only needed with construtor function 
 	}
 
-	click(value) {
-		console.log(value);
+	bindClickConstructor() {
+		console.log("bindClickConstructor ", this);
+	}
+
+	bindClick(value) {
+		console.log("bindClick ", value, this);
+	}
+
+	arrowClick(value) {
+		console.log("arrow function ", value, this);
 		this.setState({
 			count: this.state.count + value
 		});
 	}
 
 	render() {
+		//console.log(this); //still have (this) without constructor
 		return (
-		<div onClick={() => this.click(10)}>
-			{this.props.text} {this.state.count}
+		<div>	
+			<h3>{this.props.text} {this.state.count}</h3>
+			<hr/>
+			<div onClick={() => this.arrowClick(10)}>
+				() => this.arrowClick(10)
+			</div>
+			<br/>
+			<div onClick={this.bindClick.bind(this, 20)}>
+				this.bindClick.bind(this, 20);
+			</div>
+			<br/>
+
+			<div onClick={this.bindClickConstructor}>
+				this.bindClickConstructor
+			</div>
 		</div>
 		);
 	}
 }
-
+//define outside of property
 Counter.defaultProps = {
   text: 'Hello World'
 }
